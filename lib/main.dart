c938e16b1e59c2b279b6ecdbba0e7c6afcece54f
SERGE JUN <sergelebon07@gmail.com>
2024-10-04 16:00:10 +0000

    mise en place de l'Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -35,86 +35,50 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text(
-          "Les widget de bases",
-          style: TextStyle(color: Colors.white),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text(
+            "Les widget de bases",
+            style: TextStyle(color: Colors.white),
+          ),
+          leading: Icon(Icons.home),
+          actions: [
+            Icon(Icons.search),
+          ],
+          elevation: 50,
+          centerTitle: true,
         ),
-        leading: Icon(Icons.home),
-        actions: [
-          Icon(Icons.search),
-        ],
-        elevation: 50,
-        centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Divider(
-            height: 10,
-            thickness: 1,
-            indent: 1,
-            endIndent: 2,
-            color: Colors.black,
-          ),
-
-          Text("Salut de base de base de base"),
-          const Divider(
-            height: 10,
-            thickness: 1,
-            indent: 1,
-            endIndent: 2,
-            color: Colors.black,
-          ),
-          Spacer(),
-          Container(
-            color: Colors.black45,
-            height: 32,
-            child: Center(
-              child: Text("C'est une container rouge"),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 65,
+              margin: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.lightGreenAccent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                "Merci de me Contacter",
+                style: TextStyle(color: Colors.black38, fontSize: 25),
+              ),
             ),
-          ),
-          Container(
-            child: const Center(
-              child: Text("C'est un container jaune"),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.lightGreen,
+                      Colors.blue,
+                    ],
+                    begin: Alignment.topCenter,
+                  ),
+                ),
+              ),
             ),
-          ),
-          Container(
-            color: Colors.black45,
-            height: 32,
-            child: Center(
-              child: Text("C'est une container rouge"),
-            ),
-          ),
-          //const SizedBox(height: 16),
-          Container(
-            child: const Center(
-              child: Text("C'est un container jaune"),
-            ),
-          ),
-          Container(
-            color: Colors.black45,
-            height: 32,
-            child: const Center(
-              child: Text("C'est un container normalement blancs"),
-            ),
-          ),
-          Container(
-            child: const Center(
-              child: Text("C'est un container jaune"),
-            ),
-          ),
-          const SizedBox(height: 16),
-          Icon(
-            Icons.add_a_photo,
-            color: Colors.orange,
-            size: 50,
-          ),
-          Spacer(),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
